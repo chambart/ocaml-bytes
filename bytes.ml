3d79b838b6126236b574fbef4088e20a40e26967
Pierre Chambart <pierre.chambart@ocamlpro.com>
2014-05-23 14:21:30 +0200

    add functions missing from String

--- bytes.ml
+++ bytes.ml
@@ -1,1 +1,8 @@
 include String
+
+let sub_string = sub
+let empty = ""
+let of_string x = x
+let to_string x = x
+external unsafe_to_string : t -> string = "%identity"
+external unsafe_of_string : string -> t = "%identity"
--- bytes.mli
+++ bytes.mli
@@ -1,1 +1,8 @@
 include module type of String
+
+val empty : t
+val of_string : string -> t
+val to_string : t -> string
+val sub_string : t -> int -> int -> string
+external unsafe_to_string : t -> string = "%identity"
+external unsafe_of_string : string -> t = "%identity"
