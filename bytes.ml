a4bbad1322af8c922267cc6f030efd5f477b2274
Pierre Chambart <pierre.chambart@ocamlpro.com>
2014-07-10 16:05:50 +0200

    Bytes.{to,from}_string have the same semantics (copy) as stdlib one.

--- bytes.ml
+++ bytes.ml
@@ -2,7 +2,7 @@ include String
 
 let sub_string = sub
 let empty = ""
-let of_string x = x
-let to_string x = x
+let of_string x = String.copy x
+let to_string x = String.copy x
 external unsafe_to_string : t -> string = "%identity"
 external unsafe_of_string : string -> t = "%identity"
