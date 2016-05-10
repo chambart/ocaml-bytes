13dc8837c5a3dd794a5e6058eb073f4628f72f0c
Pierre Chambart <pierre.chambart@ocamlpro.com>
2016-05-10 11:40:31 +0200

    Add some missing functions

--- bytes.ml
+++ bytes.ml
@@ -6,3 +6,18 @@ let of_string x = String.copy x
 let to_string x = String.copy x
 external unsafe_to_string : t -> string = "%identity"
 external unsafe_of_string : string -> t = "%identity"
+let extend s left right =
+  let len = length s + left + right in
+  let r = create len in
+  let (srcoff, dstoff) = if left < 0 then -left, 0 else 0, left in
+  let cpylen = min (length s - srcoff) (len - dstoff) in
+  if cpylen > 0 then unsafe_blit s srcoff r dstoff cpylen;
+  r
+let blit_string = blit
+let cat s1 s2 =
+  let l1 = length s1 in
+  let l2 = length s2 in
+  let r = create (l1 + l2) in
+  unsafe_blit s1 0 r 0 l1;
+  unsafe_blit s2 0 r l1 l2;
+  r
--- bytes.mli
+++ bytes.mli
@@ -6,3 +6,6 @@ val to_string : t -> string
 val sub_string : t -> int -> int -> string
 external unsafe_to_string : t -> string = "%identity"
 external unsafe_of_string : string -> t = "%identity"
+val extend : t -> int -> int -> t
+val blit_string : string -> int -> t -> int -> int -> unit
+val cat : t -> t -> t
